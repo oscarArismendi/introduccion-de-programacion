893e98596326f3678c3b8e3760e59e606401a0a1
oscarArismendi <58527817+oscarArismendi@users.noreply.github.com>
2023-12-20 17:01:55 -0500

    Update ListasPython.py

--- ejercicios/ListasPython.py
+++ ejercicios/ListasPython.py
@@ -1,5 +1,3 @@
-# # # Ejercicio 1 expresiones con listas
-# # # Considere las siguientes listas:
 #              0, 1, 4, 5, 9
 # # # >>> a = [5, 1, 4, 9, 0]
 # # # >>> b = list(range(3, 10)) + list(range(20, 23)) -> [3,4,5,6,7,8,9] + [20,21,22] = [3,4,5,6,7,8,9,20,21,22]
@@ -73,16 +71,128 @@
 
 # # # Ejercicio 3 Mayores que el promedio
 # # # Escriba un programa que pregunte al usuario cuántos datos ingresará, a continuación le pida que ingrese los datos uno por uno, y finalmente entregue como salida cuántos de los datos ingresados son mayores que el promedio.
-n = int(input("Cuantos datos ingresara? "))
+# n = int(input("Cuantos datos ingresara? "))
+# lista = []
+# promedio = 0.0
+# for i in range (1,n+1,1): #pedimos los numeros
+#     x = float(input("Dato {}: ".format(i)))
+#     lista.append(x)
+#     promedio += x
+# promedio /= n
+# cnt = 0 # contador de elementos mayores al promedio
+# for x in lista:
+#     if x > promedio:
+#         cnt += 1
+# print("{} datos son mayores que el promedio".format(cnt))
+# # # Ejercicio 4 iguales o distintos
+# # # 1) Escriba la función todos_iguales(lista) que indique si todos los elementos de una lista son iguales:
+# # # 2) A continuación, escriba una función todos_distintos(lista) que indique si todos los elementos de una lista son distintos:
+# # # Sus funciones deben ser capaces de aceptar listas de cualquier tamaño y con cualquier tipo de datos:
+
+# def todos_iguales(lista): 
+#     primero = lista[0]#tomamos el primer valor
+#     rta = True
+#     for x in lista:
+#         if primero != x: #si algun valor en la lista es distinto al primero todos no son iguales
+#             rta = False
+#             break
+#     return rta
+
+# def todos_distintos(lista):
+#     lista.sort() # ordenamos la lista
+#     n = len(lista)
+#     rta = True
+#     if n <= 1: # el caso de que solo haya 1 o menos digitos
+#         return rta 
+#     for i in range (0,n-1,1):
+#         if lista[i] == lista[i+1]:#si encontramos algun elemento igual ya no todos son distintos
+#             rta = False
+#             break
+#     return rta
+# print(todos_iguales([6,6,6]))
+# print(todos_iguales([6,6,1]))
+# print(todos_iguales([0,90,1]))
+# print(todos_distintos([6,6,6]))
+# print(todos_distintos([6,6,1]))
+# print(todos_distintos([0,90,1]))
+# print(todos_iguales([7, 7, 7, 7, 7, 7, 7, 7, 7]))
+# print(todos_distintos(list(range(1000))))
+# print(todos_iguales([12]))
+# print(todos_distintos(list('hiperblanduzcos')))
+
+# # # Ejercicio 5 Estadisticos de localizacion
+# # # 1)La media aritmética (o promedio) de un conjunto de datos es la suma de los valores dividida por la cantidad de datos.
+# # # Escriba la función media_aritmetica(datos), donde datos es una lista de números, que entregue la media aritmética de los datos:
+# # # 2)La media armónica de un conjunto de datos es el recíproco de la suma de los recíprocos de los datos, multiplicada por la cantidad de datos:
+# # # Escriba la función media_armonica(datos), que entregue la media armónica de los datos:
+# # # 3)La mediana de un conjunto de datos reales es el valor para el que el conjunto tiene tantos datos mayores como menores a él.
+
+# # # Más rigurosamente, la mediana es definida de la siguiente manera:
+
+# # # - si la cantidad de datos es impar, la mediana es el valor que queda en la mitad al ordenar los datos de menor a mayor;
+# # # - si la cantidad de datos es par, la mediana es el promedio de los dos valores que quedan al centro al ordenar los datos de menor a mayor.
+
+# # # Escriba la función mediana(datos), que entregue la mediana de los datos:
+# # # La función no debe modificar la lista que recibe como argumento:
+# # # 4)La moda de un conjunto de datos es el valor que más se repite.
+# # # Escriba la función modas(datos), donde datos es una lista, que entregue una lista con las modas de los datos:
+# # # Usando las funciones definidas anteriormente, escriba un programa que:
+
+# # # - pregunte al usuario cuántos datos ingresará,
+# # # - le pida que ingrese los datos uno por uno, y
+# # # - muestre un reporte con las medias aritmética y armónica, la mediana y las modas de los datos ingresados.
+
+# # # Si alguno de los datos es cero, el reporte no debe mostrar la media armónica.
+def media_aritmetica(lista):
+    n = len(lista) #cantidad de datos en la lista
+    suma_total = 0.0
+    for x in lista: #ciclo para sumar todos los datos de la lista
+        suma_total += x
+    return suma_total/n
+# print(media_aritmetica([6, 1, 4, 8]))
+def media_armonica(lista):
+    n = len(lista) #cantidad de datos en la lista
+    suma_reciprocos = 0.0
+    for x in lista: #ciclo para sumar los reciprocos de la lista
+        if 
+        suma_reciprocos += 1/x
+    return n/suma_reciprocos
+# print(media_armonica([6,1,4,8]))
+def mediana(lista):
+    n = len(lista)
+    lista2 = sorted(lista) #una segunda lista para no cambiar la primera lista
+    if n % 2 != 0:#si la cantidad de datos son impares
+        return lista2[int(n/2)] #el valor que queda en el centro
+    else: # si la cantidad de datos son pares
+        return (lista2[int(n/2)]+lista2[int(n/2)-1])/2 #promedio de los dos valores que quedan en el centro
+    
+# print(mediana([5.0, 1.4, 3.2]))
+# print(mediana([5.0, 1.4, 3.2, 0.1]))
+# x = [5.0, 1.4, 3.2]
+# print(mediana(x))
+# print(x)
+
+def modas(lista):
+    max_value = max(lista)
+    contador_valores = []
+    for i in range(0,max_value+1,1): #creo una lista con solo ceros
+        contador_valores.append(0)
+    contador_maximo = 0
+    for x in lista: #busco cuanto es lo maximo que se repite un dato
+        contador_valores[x] += 1
+        if contador_valores[x] > contador_maximo:
+            contador_maximo = contador_valores[x]
+    rta = []   
+    for i in range(0,max_value+1,1):#creo la lista de los valores que mas se repiten
+        if contador_valores[i] == contador_maximo:
+            rta.append(i)
+    return rta
+
+# print(modas([5, 4, 1, 4, 3, 3, 4, 5, 0]))
+# print(modas([5, 4, 1, 4, 3, 3, 4, 5, 3]))
+# print(modas([5, 4, 5, 4, 3, 3, 4, 5, 3]))
+n = int(input("Ingrese la cantidad de datos de la lista: "))
 lista = []
-promedio = 0.0
-for i in range (1,n+1,1): #pedimos los numeros
-    x = float(input("Dato {}: ".format(i)))
+for i in range(0,n,1):
+    x = int(input("Ingrese el dato {}".format(i)))
     lista.append(x)
-    promedio += x
-promedio /= n
-cnt = 0 # contador de elementos mayores al promedio
-for x in lista:
-    if x > promedio:
-        cnt += 1
-print("{} datos son mayores que el promedio".format(cnt))
